0944f6ad7f22acff20decc7236dda383aa1c51cd
nao-webdev <209189410+nao-webdev@users.noreply.github.com>
2025-05-07 23:48:10 +0900

    Laravel Breezeでログイン機能を実装

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      host: env.VITE_HOST || 'localhost',
+      port: parseInt(env.VITE_PORT) || 5173,
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
     ],
+  };
 });
